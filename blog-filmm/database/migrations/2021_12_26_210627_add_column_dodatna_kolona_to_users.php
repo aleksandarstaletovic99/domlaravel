f8222fd78c195bca0fe5b29c919822d5c6fd67c4
Aleksandar <aleksandarstaletovic99>
2021-12-28 12:45:12 +0100

    Neke dodatne izmene na migracijama

--- blog-filmm/database/migrations/2021_12_26_210627_add_column_dodatna_kolona_to_users.php
+++ blog-filmm/database/migrations/2021_12_26_210627_add_column_dodatna_kolona_to_users.php
@@ -14,7 +14,7 @@ class AddColumnDodatnaKolonaToUsers extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            //
+            $table->integer('dodatna_kolona');
         });
     }
 
@@ -26,7 +26,7 @@ class AddColumnDodatnaKolonaToUsers extends Migration
     public function down()
     {
         Schema::table('users', function (Blueprint $table) {
-            //
+            $table->dropColumn('dodatna_kolona');
         });
     }
 }
--- blog-filmm/database/migrations/2021_12_28_114133_change_nesto_type_in_users_table.php
+++ blog-filmm/database/migrations/2021_12_28_114133_change_nesto_type_in_users_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class ChangeDodatnaKolonaToBespotrebaKolonaInUsersTable extends Migration
+class ChangeNestoTypeInUsersTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,8 +13,8 @@ class ChangeDodatnaKolonaToBespotrebaKolonaInUsersTable extends Migration
      */
     public function up()
     {
-        Schema::table('bespotreba_kolona_in_users', function (Blueprint $table) {
-            //
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('nesto')->change();
         });
     }
 
@@ -25,8 +25,8 @@ class ChangeDodatnaKolonaToBespotrebaKolonaInUsersTable extends Migration
      */
     public function down()
     {
-        Schema::table('bespotreba_kolona_in_users', function (Blueprint $table) {
-            //
+        Schema::table('users', function (Blueprint $table) {
+            $table->integer('nesto')->change();
         });
     }
 }
